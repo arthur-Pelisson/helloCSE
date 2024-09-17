984542e05c8db7d3bc5cc4fa33f0b8845a8551c4
luci <pelisson.arthur@gmail.com>
2024-09-17 23:52:06 +0200

    feat: add route for profil entity

--- helloCSE/routes/api.php
+++ helloCSE/routes/api.php
@@ -3,13 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\ProfilController;
 
 // route de test pour l'application helloCSE pour savoir si l'pplication run correctement
 Route::get('/test', function () {
     return response()->json(['test'=>'ok'], 200);
 });
 
-//prefixe pour les routes d'authentification
+// prefixe pour les routes d'authentification
 Route::prefix('auth')->group(function () {
   // route pour l'authentification des administrateurs
   Route::post('register',[AdminAuthController::class,'register']);
@@ -17,3 +18,14 @@ Route::prefix('auth')->group(function () {
   Route::post('logout',[AdminAuthController::class,'logout'])
     ->middleware('auth:sanctum');
 });
+
+// route protected by sanctum middleware
+Route::middleware('auth:sanctum')->group(function () {
+  // route for profil
+  Route::post('profil',[ProfilController::class,'store']);
+  Route::put('profil',[ProfilController::class,'update']);
+  Route::delete('profil',[ProfilController::class,'destroy']);
+});
+
+// route profil not protected
+Route::get('profil',[ProfilController::class,'index']);
