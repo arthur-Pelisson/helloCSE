88c73af630c2f3f6598bf314c4f515e327302887
luci <pelisson.arthur@gmail.com>
2024-09-17 23:49:10 +0200

    feat: add fillable data for profil model

--- helloCSE/app/Models/profil.php
+++ helloCSE/app/Models/profil.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class profil extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        "image",
+        "statut",
+    ];
+    
 }
